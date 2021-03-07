bb53d4e2fd079ed4177f7fe3ce658ef8c6385100
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-03-07 15:14:38 -0700

    add Events page

---
+++ src/routes/events/Event.svelte
@@ -0,0 +1,56 @@
+<script>
+  import EventImage from './img/event.svg';
+  let moreInformationUrl = '#';
+  let description =
+    'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ';
+</script>
+
+<article>
+  <div>
+    <img src={EventImage} alt="Event" />
+  </div>
+  <div class="Event-content">
+    <div>
+      <p>{description}</p>
+    </div>
+    <div class="Event-content-actions">
+      <a href={moreInformationUrl}>Mas informacion</a>
+      <button>Participar</button>
+    </div>
+  </div>
+</article>
+
+<style>
+  article {
+    display: grid;
+    box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.3);
+    border-radius: 5px;
+  }
+  article div img {
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+  }
+  .Event-content {
+    padding: 10px;
+  }
+  .Event-content p {
+    margin-bottom: 10px;
+  }
+  .Event-content-actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .Event-content-actions button {
+    background-color: var(--color-primary-1);
+    color: white;
+    border-radius: 5px;
+    padding: 10px 20px;
+  }
+  /* .Event-content-actions button:hover{
+
+  } */
+  .Event-content-actions a {
+    color: #636bfb;
+  }
+</style>
--- src/routes/events/index.svelte
+++ src/routes/events/index.svelte
@@ -1,5 +1,6 @@
 <script>
   import LayoutBase from '../../components/Layout/Layout_Base.svelte';
+  import Event from './Event.svelte';
   let page = 'events';
 </script>
 
@@ -8,8 +9,27 @@
 </svelte:head>
 
 <LayoutBase {page}>
-  <h1>Events</h1>
+  <h1>Eventos activos</h1>
+  <main>
+    <Event />
+    <Event />
+    <Event />
+    <Event />
+    <Event />
+    <Event />
+  </main>
 </LayoutBase>
 
 <style>
+  h1 {
+    text-align: center;
+    font-size: 20px;
+    margin-top: 20px;
+  }
+  main {
+    display: grid;
+    grid-template-columns: auto auto auto;
+    grid-gap: 20px;
+    margin: 50px 10px;
+  }
 </style>
